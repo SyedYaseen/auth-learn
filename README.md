f8282e7def25c01c0ddcb2cfbc353106853413ac
TheRealYaseen <s.syedyaseen.s@gmail.com>
2024-03-17 23:21:56 +0000

    changes

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+# Commands
+docker run -d -p 27017:27017 --name auth-mongo mongo:latest
+
+
 # auth-learn
 1. Authentication - Who the user is?
 2. Authorization - Who has access to what? Ex. Google sign in
--- middleware.js
+++ middleware.js
@@ -4,31 +4,41 @@ const port = 3000
 
 app.use(middleware1)
 app.use(middleware2)
+app.use(middleware3)
+
+
+function errHandler(err, req, res, next) {
+    if (err) res.send("Sorry there as an err")
+}
 
 function middleware1 (req, res, next) {
-    console.log("I am middleware 1")
+    res.test = 100
     next()
 }
 
 function middleware2 (req, res, next) {
-    console.log("I am middlware 2")
+
+    console.log("I am middlware 2", res.test)
+    res.test= 200
     next()
 }
 
 function middleware3 (req, res, next) {
-    console.log("I am middlware 3")
+    console.log("I am middlware 3", res.test)
+    res.test = 300
     next()
 }
 
-app.get('/', middleware1, (req, res, next) => {
+app.get('/', (req, res, next) => {
     console.log("I am standard express")
-    res.send('This works')
+    res.send(`This works ${res.test}`)
 })
 
 app.get('/test', (rer, res, next) => {
     res.send("This is the test page")
 })
 
+app.use(errHandler)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
