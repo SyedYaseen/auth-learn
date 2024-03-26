a41b3e633e0c1684d8153798262309226caf22fa
TheRealYaseen <s.syedyaseen.s@gmail.com>
2024-03-26 11:07:40 +0000

    changes

--- express-jwt-authentication-starter/config/passport.js
+++ express-jwt-authentication-starter/config/passport.js
@@ -1,12 +1,30 @@
-const fs = require('fs');
-const path = require('path');
-const User = require('mongoose').model('User');
+const fs = require("fs")
+const path = require("path")
+const User = require("mongoose").model("User")
+const { ExtractJwt, JwtStrategy } = require("passport-jwt")
 
-const pathToKey = path.join(__dirname, '..', 'id_rsa_pub.pem');
-const PUB_KEY = fs.readFileSync(pathToKey, 'utf8');
-
-// TODO
-const options = {};
+const pathToKey = path.join(__dirname, "..", "id_rsa_pub.pem")
+const PUB_KEY = fs.readFileSync(pathToKey, "utf8")
 
 // TODO
-module.exports = (passport) => {}
+const options = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: PUB_KEY,
+  algorithms: ["RSA256"],
+}
+
+const strategy = new JwtStrategy(options, (payload, done) => {
+  User.findOne({ _id: payload.sub })
+    .then((user) => {
+      if (user) return done(null, user)
+      else return done(null, false)
+    })
+    .catch((err) => {
+      done(err, false)
+    })
+})
+
+// TODO
+module.exports = (passport) => {
+  passport.use(strategy)
+}
